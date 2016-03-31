d825a4b7788f95ccb8fbd3d3d65428b0d130d85c
dreyk <gunin@satissoft.ru>
2016-03-31 11:18:42 +0300

    Generate Broker ID by POD ip

--- kafka-autoextend-partitions.sh
+++ kafka-autoextend-partitions.sh
@@ -7,17 +7,19 @@ if [[ -z "$START_TIMEOUT" ]]; then
     START_TIMEOUT=600
 fi
 
-start_timeout_exceeded=false
-count=0
-step=10
-while netstat -lnt | awk '$4 ~ /:'$KAFKA_PORT'$/ {exit 1}'; do
-    echo "waiting for kafka to be ready"
-    sleep $step;
-    count=$(expr $count + $step)
-    if [ $count -gt $START_TIMEOUT ]; then
-        start_timeout_exceeded=true
-        break
-    fi
-done
+if [[ "$1" = "up" ]];then
+	start_timeout_exceeded=false
+	count=0
+	step=10
+	while netstat -lnt | awk '$4 ~ /:'$KAFKA_PORT'$/ {exit 1}'; do
+    	echo "waiting for kafka to be ready"
+    	sleep $step;
+    	count=$(expr $count + $step)
+    	if [ $count -gt $START_TIMEOUT ]; then
+        	start_timeout_exceeded=true
+        	break
+    	fi
+	done
+fi
 
 exec $KAFKA_HOME/bin/kafka-run-class.sh kafka.admin.AutoExpandCommand --zookeeper=$KAFKA_ZOOKEEPER_CONNECT --broker=$BROKER --updown=$1
--- kafka-kubernetes-start.sh
+++ kafka-kubernetes-start.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
-echo "Generate Kafka Broker ID"
-function join { local IFS="$1"; shift; echo "$*"; }
-A=(${KAFKA_ADVERTISED_HOST_NAME//\./ })
-ID=`join '' ${A[@]:1}`
-ID=${ID##*0}
-echo "Use broker ID: $ID"
-
-export KAFKA_BROKER_ID=$ID
+if [[ -n "$BROKER_ID_PROTOTYPE" ]]; then
+    echo "Generate Kafka Broker ID"
+	function join { local IFS="$1"; shift; echo "$*"; }
+	A=(${BROKER_ID_PROTOTYPE//\./ })
+	ID=`join '' ${A[@]:1}`
+	ID=${ID##*0}
+	echo "Use broker ID: $ID"
+	export KAFKA_BROKER_ID=$ID
+fi
 
 /usr/bin/start-kafka.sh
 
