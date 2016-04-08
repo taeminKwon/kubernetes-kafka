7f94599e5760e8d6994675c26648b4b3073e8210
dreyk <gunin@satissoft.ru>
2016-04-08 22:34:25 +0300

    Validate kafka IP

--- kafka-kubernetes-start.sh
+++ kafka-kubernetes-start.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-if [[ -n "$BROKER_ID_PROTOTYPE" ]]; then
+if ! [[  $KAFKA_ADVERTISED_HOST_NAME =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+	echo "KAFKA_ADVERTISED_HOST_NAME NOT DEFINED OR INVALID '$KAFKA_ADVERTISED_HOST_NAME' !!!"
+	exit 1
+fi
+
+if [[ -n "$BROKER_ID_PROTOTYPE" && -z "$KAFKA_BROKER_ID" ]]; then
     echo "Generate Kafka Broker ID: for $BROKER_ID_PROTOTYPE"
 	function join { local IFS="$1"; shift; echo "$*"; }
 	A=(${BROKER_ID_PROTOTYPE//\./ })
@@ -10,7 +15,10 @@ if [[ -n "$BROKER_ID_PROTOTYPE" ]]; then
 	export KAFKA_BROKER_ID=$ID
 fi
 
-trap "echo 'Rebalancing'; /usr/bin/kafka-autoextend-partitions.sh down; exit" SIGHUP SIGINT SIGTERM
-/usr/bin/kafka-autoextend-partitions.sh up &
-/usr/bin/start-kafka.sh
-
+if [[ -n "$ENABLE_AUTO_EXTEND" ]]; then
+	trap "echo 'Rebalancing'; /usr/bin/kafka-autoextend-partitions.sh down; exit" SIGHUP SIGINT SIGTERM
+	/usr/bin/kafka-autoextend-partitions.sh up &
+	/usr/bin/start-kafka.sh
+else
+	/usr/bin/start-kafka.sh
+fi
